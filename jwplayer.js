172e47b3da6605364652d7423f6f5b4bf6693a0e
ibommal <ibommalofficial@gmail.com>
2025-03-23 22:20:55 +0530

    Create jwplayer.js

---
+++ jwplayer.js
@@ -0,0 +1,143 @@
+.jw-slider-horizontal.jw-chapter-slider-time
+  .jw-slider-container
+  .jw-timesegment-progress,
+.jw-progress {
+  background-color: #ffff0a !important;
+}
+
+.jwplayer {
+  width: 100% !important;
+  border-radius: 5px;
+  height: 680px !important;
+}
+.jw-icon-rewind {
+  display: flex !important;
+}
+.jw-slider-horizontal.jw-chapter-slider-time .jw-old-rail {
+  /* backface-visibility: visible !important;
+  visibility: visible !important; */
+}
+.jwplayer.jw-stretch-uniform .jw-media video {
+  /* object-fit: fill !important; */
+}
+.jw-old-rail:has(.jw-timesegment) {
+  background-color: transparent !important;
+  height: 100% !important;
+}
+.jw-old-rail:has(.jw-timesegment):hover {
+  height: 100% !important;
+  background-color: transparent !important;
+}
+.jw-old-rail:hover > .jw-timesegment {
+  background-color: transparent !important;
+}
+.jw-old-rail {
+  background: rgba(255, 255, 255, 0.3) !important;
+  height: 3px !important;
+}
+.jw-old-rail:hover {
+  background: rgba(255, 255, 255, 0.49) !important;
+  height: 5px !important;
+}
+.init-width {
+  width: 100%;
+}
+.jw-icon-forward {
+  color: rgba(255, 255, 255, 0.8) !important;
+  opacity: 0.8 !important;
+}
+.jw-icon-forward:hover {
+  opacity: 1 !important;
+  color: rgba(255, 255, 255, 1) !important;
+}
+.jw-icon-forward:hover .jw-tooltip-forward {
+  opacity: 1 !important;
+  transform: translate(-50%, -10px) !important;
+  transition-duration: 150ms !important;
+  transition-delay: 0.5s, 0s, 0.5s !important;
+  visibility: visible !important;
+}
+.jw-breakpoint-7:not(.jw-flag-audio-player)
+  .jw-controlbar
+  .jw-button-container {
+  padding-bottom: 0 !important;
+}
+/* .jw-video.jw-reset {
+  visibility: hidden !important;
+} */
+@media (min-width: 768px) {
+  .init-width {
+    max-width: 1200px;
+    margin: auto;
+  }
+}
+@media (max-width: 1100px) {
+  .jwplayer {
+    height: 600px !important;
+  }
+}
+@media (max-width: 950px) {
+  .jwplayer {
+    height: 500px !important;
+  }
+}
+@media (max-width: 700px) {
+  .jwplayer {
+    height: 380px !important;
+  }
+}
+@media (max-width: 600px) {
+  .jwplayer {
+    height: 330px !important;
+  }
+  .jw-icon-pip {
+    display: none;
+  }
+}
+@media (max-width: 750px) {
+  .jwplayer {
+    height: 300px !important;
+  }
+}
+@media (max-width: 500px) {
+  .jwplayer {
+    height: 270px !important;
+  }
+}
+@media (max-width: 450px) {
+  .jwplayer {
+    height: 250px !important;
+  }
+}
+@media (max-width: 400px) {
+  .jwplayer {
+    height: 220px !important;
+  }
+}
+@media (max-width: 350px) {
+  .jwplayer {
+    height: 200px !important;
+  }
+}
+@media (max-width: 300px) {
+  .jwplayer {
+    height: 170px !important;
+  }
+}
+.jw-rightclick.jw-reset.jw-open{
+  display: none !important;
+}
+.noma,
+.nav-square,
+::marker {
+  display: none !important;
+}
+
+.jw-rightclick.jw-reset.jw-open{
+  display: none !important;
+}
+.noma,
+.nav-square,
+::marker {
+  display: none !important;
+}
